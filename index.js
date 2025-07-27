62bf22033884cde491825fb80f7c065d6610aba0
PrateekChand <your-email@wisc.edu>
2025-07-27 14:10:59 -0500

    Added routers

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const express = require('express')
 
+// a more tedious way of using route handling
+//const {createUserRoutes} = require("./routes/user")
+//const {createCourseRoutes} = require("./routes/course")
+
+const {useRouter} = require("./routes/user")
+const {courseRouter} = require("./routes/course")
+
+// Express router is a powerful feature that allows you to create modular, mountable route handlers.
+// It is initially a mini-application that can handle middleware and routes.
+
 const app = express();
 
+// if any of the request come to use endpoint it automatically get routed to userRouter
+app.use("/user", useRouter)
+app.use("/course", courseRouter)
+
+//createUserRoutes(app);
+//createCourseRoutes(app);
+
+/*
 app.post('/user/signup', function(req, res){
     res.json({
         message: 'signup endpoint'
@@ -30,10 +48,12 @@ app.post('/course/purchase', function(req, res){
 
 
 // Lists all the current courses
-app.get('/courses', function(req, res){
+app.get('/course/preview', function(req, res){
     res.json({
         message: 'signin endpoint'
     })
 });
+
+*/
 
 app.listen(3000);
